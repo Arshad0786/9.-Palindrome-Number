60b74b27ebf24a7f404da1583496d38902baef81
Arshad0786 <js07150715@gmail.com>
2020-03-09 18:13:11 +0800

    finish logics of main file and add basic unittests

--- PalindromeNumber.py
+++ PalindromeNumber.py
@@ -1,3 +1,15 @@
 class Solution:
     def isPalindrome(self, x):
-        pass
+        reverseNumber = 0
+        tempX = int(x)  
+        if(x < 0):
+            return False
+
+        for i in range(len(str(x))):
+            reverseNumber = reverseNumber * 10
+            reverseNumber = reverseNumber + tempX % 10
+            tempX = tempX // 10
+
+        if(reverseNumber == x):
+            return True
+        return False
--- test.py
+++ test.py
@@ -5,9 +5,35 @@ from PalindromeNumber import Solution
 class ReverseIntegerTest(unittest.TestCase):
     def test_basic_function(self):
         temp = Solution()
-        self.value = 121 
-        self.assertEqual(temp.isPalindrome(self.value), True) 
+        self.value = 121
+        self.assertEqual(temp.isPalindrome(self.value), True)
+
+    def test_only_two_digits(self):
+        temp = Solution()
+        self.value = 11
+        self.assertEqual(temp.isPalindrome(self.value), True)
+
+    def test_long_number(self):
+        temp = Solution()
+        self.value = 12345678987654321
+
+        self.assertEqual(temp.isPalindrome(self.value), True)
     
+    def test_negative_number(self):
+        temp = Solution()
+        self.value = -121
+        self.assertEqual(temp.isPalindrome(self.value), False)
+        
+    def test_two_digits_false(self):
+        temp = Solution()
+        self.value = 12
+        self.assertEqual(temp.isPalindrome(self.value), False)
+    
+    def test_input_one_digit(self):
+        temp = Solution()
+        self.value = 0
+        self.assertEqual(temp.isPalindrome(self.value), True)
+
 
 if __name__ == "__main__":
     unittest.main()
